7ab74200c3007cff2371497a0c6450d8a476f66c
Bhuvanesh-J <bhuvanesh@divum.in>
2022-10-19 03:04:32 +0530

    pdf read done

--- api/search/views.py
+++ api/search/views.py
@@ -1,6 +1,8 @@
 import json
 import traceback
 from datetime import datetime
+import uuid
+from PyPDF2 import PdfFileReader
 
 from flask import Blueprint
 from flask import request
@@ -13,18 +15,38 @@ es_search = Blueprint('search', __name__, url_prefix='/api/v1/es/')
 def insert_data():
     try:
         from app import es
-        slug = 'file_name'
+        mapping = { "settings": { "number_of_shards": 2, "number_of_replicas": 1 }, "mappings": { "properties": { "content": { "type": "text" } } } }
+        response = es.indices.create(
+            index="some_new_index",
+            body=mapping,
+            ignore=400  # ignore 400 already exists code
+        )
+        file_obj = request.files['file']
+        filename=file_obj.filename
+        file_format=filename.split('.')
+        pdf = PdfFileReader(file_obj)
+        # content_list=[]
+        text=''
+        for page in range(pdf.getNumPages()):
+            text+=pdf.getPage(page).extractText()
+            # content_list.append({
+            #     'page_'+str(page):text
+            # })
+
+        id = uuid.uuid4()
         body = {
-            'slug': slug,
-            'title': 'search_something',
-            'content': 'byte_data',
+            'id': id,
+            'file_name': filename,
+            'content': text,
+            'file_type':file_format[-1],
             'timestamp': datetime.now()
         }
 
-        result = es.index(index='contents', id=slug, body=body,request_timeout=30)
+        result = es.index(index='some_new_index', id=id, body=body)
+        # print(result)
         # result = es.index(index='content', id=slug, body=body)
 
-        return success({"result": ""}, "Successfully created")
+        return success({"result": body}, "Successfully created")
     except Exception as e:
         # print(traceback.format_exc())
         return failed({}, str(e))
@@ -60,11 +82,11 @@ def search():
             "query": {
                 "multi_match": {
                     "query": keyword,
-                    "fields": ["content", "title"]
+                    "fields": ["content",'file_name','file_type','data','content']
                 }
             }
         }
-        res = es.search(index="contents", body=body,request_timeout=30)
+        res = es.search(index="some_new_index", body=body)
         data = res['hits']['hits']
         return success({"result": data}, "Successfully retrieved")
     except Exception as e:
